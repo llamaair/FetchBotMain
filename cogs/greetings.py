3154b34b2407dc6a1299fecd03e0ba970b03ef83
llamaair <83296140+llamaair@users.noreply.github.com>
2023-01-10 10:59:20 +0100

    bug fix

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class Greetings(commands.Cog): # create a class for our cog that inherits from commands.Cog
     # this class is used to create a cog, which is a module that can be added to the bot
 
-    def __init__(bot, client): # this is a special method that is called when the cog is loaded
+    def __init__(self, bot): # this is a special method that is called when the cog is loaded
         self.bot = bot
 
     @discord.slash_command() # we can also add application commands
