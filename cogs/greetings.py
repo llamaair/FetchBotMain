86fc8bd4040efdb478fa8a6cab96cdd9bd806abc
llamaair <83296140+llamaair@users.noreply.github.com>
2023-01-10 10:58:03 +0100

    fixes

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -4,20 +4,13 @@ from discord.ext import commands
 class Greetings(commands.Cog): # create a class for our cog that inherits from commands.Cog
     # this class is used to create a cog, which is a module that can be added to the bot
 
-    def __init__(self, client): # this is a special method that is called when the cog is loaded
-        self.bot = client
-
-    @commands.command() # creates a prefixed command
-    async def hello(self, ctx): # all methods now must have both self and ctx parameters
-        await ctx.send('Hello!')
+    def __init__(bot, client): # this is a special method that is called when the cog is loaded
+        self.bot = bot
 
     @discord.slash_command() # we can also add application commands
     async def goodbye(self, ctx):
         await ctx.respond('Goodbye!')
 
-    @discord.user_command()
-    async def greet(self, ctx, member: discord.Member):
-        await ctx.respond(f'{ctx.author.mention} says hello to {member.mention}!')
 
     @commands.Cog.listener() # we can add event listeners to our cog
     async def on_member_join(self, member): # this is called when a member joins the server
