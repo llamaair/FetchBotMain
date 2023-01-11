4dddb2531490a60131c86bdea9bd994013f82a7d
llamaair <83296140+llamaair@users.noreply.github.com>
2023-01-11 16:55:53 +0100

    added more banned words

--- cogs/automod.py
+++ cogs/automod.py
@@ -35,8 +35,18 @@ class automod(commands.Cog): # create a class for our cog that inherits from com
         channel = str(message.channel.name)
         guild = str(message.guild.name)
         print(f'{username} : {user_message} ({channel}) ({guild})')
-        if 'foo' in message.content.lower():
-            await message.channel.send(f"{message.author.mention} that is a banned word :eyes:")
+        with open("automodguilds.json") as f:
+            automodguild = json.load(f)
+        if message.guild.id in automodguild:
+
+            if 'foo' in message.content.lower():
+                await message.channel.send(f"{message.author.mention} watch your mouth :eyes:")
+            if 'nigger' in message.content.lower():
+                await message.channel.send(f"{message.author.mention} watch your mouth :eyes:")
+            if 'bitch' in message.content.lower():
+                await message.channel.send(f"{message.author.mention} watch your mouth :eyes:")
+            if 'fucker' in message.content.lower():
+                await message.channel.send(f"{message.author.mention} watch your mouth :eyes:")
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(automod(bot)) # add the cog to the bot
