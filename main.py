27e36b410a77fd3abb96fda2a12cde4607c34f44
llamaair <83296140+llamaair@users.noreply.github.com>
2023-01-12 18:10:38 +0100

    d

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import os
 import json
 from discord.ext.commands import check
 from dotenv import load_dotenv
-import botdash
+from botdash.py import botdash
 
 botda = botdash.Client(
   '<O4MABIVhZfNmK0Gb1qLahpy2YLUY20xfdlP0XcZJNzZik63fcu2DXr9Irm8YnLjJ>',
