9871a96890f73e626cc47b7a1b9947e14832fbdc
llamaair <83296140+llamaair@users.noreply.github.com>
2023-01-16 12:14:04 +0100

    bug fix

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import time
-import timedelta
+from datetime import timedelta
 
 class moderation(commands.Cog): # create a class for our cog that inherits from commands.Cog
     # this class is used to create a cog, which is a module that can be added to the bot
