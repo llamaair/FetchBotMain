ddd3d683675c6b46d93014e3d97804ebce1fa87a
llamaair <83296140+llamaair@users.noreply.github.com>
2023-01-18 11:14:09 +0100

    added weather command

--- main.py
+++ main.py
@@ -60,6 +60,39 @@ def check_if_user_has_premium(ctx):
 
   return True
 
+api_key = "c08a058955da2e4ba9286a2117aa8897"
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+@client.command()
+async def weather(ctx, *, city: str):
+  city_name = city
+  complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+  response = requests.get(complete_url)
+  x = response.json()
+  channel = ctx.message.channel
+  if x["cod"] != "404":
+    async with channel.typing():
+      y = x["main"]
+      current_temperature = y["temp"]
+      current_temperature_celsiuis = str(round(current_temperature - 273.15))
+      current_pressure = y["pressure"]
+      current_humidity = y["humidity"]
+      z = x["weather"]
+      weather_description = z[0]["description"]
+      weather_description = z[0]["description"]
+      embed = discord.Embed(title=f"Weather in {city_name}",color=ctx.guild.me.top_role.color)
+      embed.add_field(name="Descripition", value=f"**{weather_description}**", inline=False)
+      embed.add_field(name="Temperature(C)", value=f"**{current_temperature_celsiuis}°C**", inline=False)
+      embed.add_field(name="Humidity(%)", value=f"**{current_humidity}%**", inline=False)
+      embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{current_pressure}hPa**", inline=False)
+      embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
+      embed.set_footer(text=f"Requested by {ctx.author.name}")
+      await channel.send(embed=embed)
+  else:
+    await channel.send("City not found.")
+
+
+
 @client.command(description="Search for a youtube video")
 @check(check_if_user_has_premium)
 async def ytsearch(ctx, *, search1):
